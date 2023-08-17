128b14b145cf86a01084d6b2c692c38038de8a5a
Sudhi Ranjan Gupta <sudhigupta190@gmail.com>
2023-08-17 16:20:15 +0530

    --fixed

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,16 @@
 import { app } from "./app";
+import { v2 as cloudinaryV2 } from "cloudinary";
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+cloudinaryV2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 app.listen(process.env.PORT || port, () => {
   console.log("connected to port", process.env.PORT);
 });
--- backend/src/Controller/userController.ts
+++ backend/src/Controller/userController.ts
@@ -3,15 +3,12 @@ import { PrismaClient } from "@prisma/client";
 import ErrorHandler from "../middleware/errorHandler";
 import bcrypt from "bcrypt";
 import { sendCookie } from "../Utils/feature";
-
+import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 const prisma = new PrismaClient();
 
 export const getRoleType = async (req: Request, res: Response, next: Next) => {
   try {
-    const dbUrl = process.env.DATABASE_URL;
     const roleTypes_ = await prisma.roleType.findMany();
-    console.log("DATABASE_URL:", dbUrl, roleTypes_);
-
     if (roleTypes_.length === 0) {
       // Create RoleTypes if not exist
       await prisma.roleType.createMany({
@@ -39,22 +36,57 @@ export const createUser = async (req: Request, res: Response, next: Next) => {
       last_name,
       phone_numbers,
       password,
-      role_type, // Assuming you provide an existing roleTypeId
-      // avatar,
+      role_type,
     } = req.body;
 
-    // Check if the user already exists
+    if (!req.files || !req.files.avatar) {
+      return next(new ErrorHandler("Please upload your Avatar", 400));
+    }
+
+    const avatars = Array.isArray(req.files.avatar)
+      ? req.files.avatar
+      : [req.files.avatar];
+
+    // console.log(email, " data ");
     const existingUser = await prisma.user.findUnique({
       where: { email },
     });
-
     if (existingUser) {
       return next(new ErrorHandler("User Already Exists", 400));
     }
 
+    const uploadedResults: UploadApiResponse[] = [];
+
+    // Upload the avatar images to Cloudinary
+    const config = {
+      folder: "avatars",
+    };
+
+    for (const avatar of avatars) {
+      const buffer = avatar.data;
+
+      const result = await new Promise<UploadApiResponse>((resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream(config, (error, result) => {
+            if (error) {
+              reject(error);
+            } else {
+              if (result) resolve(result);
+            }
+          })
+          .end(buffer);
+      });
+      if (result) uploadedResults.push(result);
+    }
     // Create phone numbers and get their IDs
+    const avatar_ = await prisma.avatar.create({
+      data: {
+        cloudinary_id: uploadedResults[0].public_id,
+        url: uploadedResults[0].secure_url,
+      },
+    });
     const createdPhoneNumbers = await Promise.all(
-      phone_numbers.map(async (phone: string) => {
+      phone_numbers.split(",").map(async (phone: string) => {
         return await prisma.phone.create({
           data: { phone_number: phone },
         });
@@ -63,35 +95,124 @@ export const createUser = async (req: Request, res: Response, next: Next) => {
 
     // Create the user and associate phone numbers and role
     const hashedPassword = await bcrypt.hash(password, 10);
-    const User = await prisma.user.create({
-      data: {
-        email,
-        first_name,
-        middle_name,
-        last_name,
-        phone_numbers: {
-          connect: createdPhoneNumbers.map((phone) => ({ id: phone.id })),
+    if (avatar_ && avatar_.id) {
+      const User = await prisma.user.create({
+        data: {
+          email,
+          first_name,
+          middle_name,
+          last_name,
+          phone_numbers: {
+            connect: createdPhoneNumbers.map((phone) => ({ id: phone.id })),
+          },
+          password: hashedPassword,
+          role_type: {
+            connect: { id: role_type },
+          },
+          avatar: {
+            connect: { id: avatar_.id },
+          },
+          // Store the Cloudinary result URLs in your user model
+          // avatarUrls: uploadedResults.map((result) => result.url),
         },
-        password: hashedPassword,
-        role_type: {
-          connect: { id: role_type }, // Connect the RoleType using role_type field
-        },
-        // avatar,
-      },
-    });
-
-    sendCookie(User, res, "Registered Successfully", 201);
-    res.status(201).json(User);
+      });
+      sendCookie(User, res, "Registered Successfully", 201);
+      // res.status(201).json(User);
+    }
+    // next(new ErrorHandler("Please provide avatar", 400));
   } catch (error) {
     next(error);
   }
 };
 
+// export const createUser1 = async (req: Request, res: Response, next: Next) => {
+//   try {
+//     const {
+//       email,
+//       first_name,
+//       middle_name,
+//       last_name,
+//       phone_numbers,
+//       password,
+//       role_type, // Assuming you provide an existing roleTypeId
+//       // avatar,
+//     } = req.body;
+
+//     if (!req.files) {
+//       return next(new ErrorHandler("No files uploaded", 400));
+//     }
+//     const avatar = req.files.avatar;
+//     // Check if the user already exists
+//     if (!avatar) {
+//       return next(new ErrorHandler("Please upload your Avatar", 400));
+//     }
+//     console.log(avatar, " AVATAR ");
+//     console.log(req.body, " data ");
+
+//     const existingUser = await prisma.user.findUnique({
+//       where: { email },
+//     });
+//     if (existingUser) {
+//       return next(new ErrorHandler("User Already Exists", 400));
+//     }
+//     // Upload the avatar image to Cloudinary
+//     const buffer = avatar.data;
+//     const config = {
+//       folder: "avatars",
+//     };
+
+//     const result: UploadApiResponse = await cloudinary.uploader.upload_stream(
+//       config,
+//       buffer
+//     );
+//     // Create phone numbers and get their IDs
+//     const createdPhoneNumbers = await Promise.all(
+//       // only for development purposes
+//       phone_numbers.split(",").map(async (phone: string) => {
+//         return await prisma.phone.create({
+//           data: { phone_number: phone },
+//         });
+//       })
+//     );
+//     // const createdPhoneNumbers = await Promise.all(
+//     //   phone_numbers.map(async (phone: string) => {
+//     //     return await prisma.phone.create({
+//     //       data: { phone_number: phone },
+//     //     });
+//     //   })
+//     // );
+
+//     // Create the user and associate phone numbers and role
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const User = await prisma.user.create({
+//       data: {
+//         email,
+//         first_name,
+//         middle_name,
+//         last_name,
+//         phone_numbers: {
+//           connect: createdPhoneNumbers.map((phone) => ({ id: phone.id })),
+//         },
+//         password: hashedPassword,
+//         role_type: {
+//           connect: { id: role_type }, // Connect the RoleType using role_type field
+//         },
+//         // avatarId: String(Avatar),
+//       },
+//     });
+
+//     sendCookie(User, res, "Registered Successfully", 201);
+//     res.status(201).json(User);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const login = async (req: Request, res: Response, next: Next) => {
   try {
     const { email, password } = req.body;
     const user = await prisma.user.findUnique({
-      where: { email },
+      where: { email: email },
     });
     if (!user) {
       return next(new ErrorHandler("User Not Found", 400));
@@ -102,7 +223,7 @@ export const login = async (req: Request, res: Response, next: Next) => {
     sendCookie(
       user,
       res,
-      `Welcome back, ${user.first_name} ${user?.middle_name} ${user.last_name}`,
+      `Welcome back, ${user?.first_name} ${user?.middle_name} ${user?.last_name}`,
       200
     );
   } catch (error) {
